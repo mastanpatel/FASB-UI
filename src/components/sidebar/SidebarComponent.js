3bfa7625234171993a1c1a4f1f086d107236ae45
Mastan Patel <m.mastanpatel@gmail.com>
2021-11-16 18:51:58 +0530

    changed the website to responsive

--- src/components/sidebar/SidebarComponent.js
+++ src/components/sidebar/SidebarComponent.js
@@ -1,77 +1,138 @@
 import React from 'react';
-import { Column } from 'simple-flexbox';
+import { Column, Row } from 'simple-flexbox';
 import { StyleSheet, css } from 'aphrodite';
 import LogoComponent from './LogoComponent';
 import MenuItemComponent from './MenuItemComponent';
 import IconOverview from '../../assets/icons/icon-overview.js';
-//import IconTickets from '../../assets/icons/icon-tickets.js';
+//import IconTickets from '../../assets/icon-tickets.js';
 import IconIdeas from '../../assets/icons/icon-ideas.js';
-//import IconContacts from '../../assets/icons/icon-contacts';
-// import IconAgents from '../../assets/icons/icon-agents';
- import IconArticles from '../../assets/icons/icon-articles';
- import IconSettings from '../../assets/icons/icon-settings';
- import IconSubscription from '../../assets/icons/icon-subscription';
+//import IconContacts from '../../assets/icon-contacts';
+//import IconAgents from '../../assets/icon-agents';
+import IconArticles from '../../assets/icons/icon-articles';
+import IconSettings from '../../assets/icons/icon-settings';
+import IconSubscription from '../../assets/icons/icon-subscription';
+import IconBurger from '../../assets/icons/icon-burger';
 
 const styles = StyleSheet.create({
+    burgerIcon: {
+        cursor: 'pointer',
+        position: 'absolute',
+        left: 24,
+        top: 34
+    },
     container: {
         backgroundColor: '#363740',
         width: 255,
-        paddingTop: 32
+        paddingTop: 32,
+        height: 'calc(100% - 32px)'
+    },
+    containerMobile: {
+        transition: 'left 0.5s, right 0.5s',
+        position: 'absolute',
+        width: 255,
+        height: 'calc(100% - 32px)',
+        zIndex: 901
+    },
+    mainContainer: {
+        height: '100%',
+        minHeight: '100vh'
+    },
+    mainContainerMobile: {
+        position: 'absolute',
+        width: '100vw',
+        minWidth: '100%',
+        top: 0,
+        left: 0
     },
     menuItemList: {
         marginTop: 52
+    },
+    outsideLayer: {
+        position: 'absolute',
+        width: '100vw',
+        minWidth: '100%',
+        height: '100%',
+        backgroundColor: 'rgba(0,0,0,.50)',
+        zIndex: 900
     },
     separator: {
         borderTop: '1px solid #DFE0EB',
         marginTop: 16,
         marginBottom: 16,
         opacity: 0.06
+    },
+    hide: {
+        left: -255
+    },
+    show: {
+        left: 0
     }
 });
 
-function SidebarComponent(props) {
-    return (
-        <Column className={css(styles.container)}>
-            <LogoComponent />
-            <Column className={css(styles.menuItemList)}>
-                <MenuItemComponent
-                    title="Manage Quarter" icon={IconOverview}
-                    onClick={() => props.onChange('Manage Quarter')}
-                    active={props.selectedItem === 'Manage Quarter'}
-                />
-                <MenuItemComponent
-                    title="Input Wizard" icon={IconSettings}
-                    onClick={() => props.onChange('Input Wizard')}
-                    active={props.selectedItem === 'Input Wizard'}
-                />
-                <MenuItemComponent
-                    title="Dashboard" icon={IconIdeas}
-                    onClick={() => props.onChange('Dashboard')}
-                    active={props.selectedItem === 'Dashboard'} />
-                <MenuItemComponent
-                    title="Reports" icon={IconArticles}
-                    onClick={() => props.onChange('Reports')}
-                    active={props.selectedItem === 'Reports'} />
-                {/* <MenuItemComponent
-                    title="Agents" icon={IconAgents}
-                    onClick={() => props.onChange('Agents')}
-                    active={props.selectedItem === 'Agents'} /> */}
-                {/* <MenuItemComponent
-                    title="Articles" icon={IconArticles}
-                    onClick={() => props.onChange('Articles')}
-                    active={props.selectedItem === 'Articles'} />
-                <div className={css(styles.separator)}></div> */}
-                {/* <MenuItemComponent
-                    title="Settings" icon={IconSettings}
-                    onClick={() => props.onChange('Settings')}
-                    active={props.selectedItem === 'Settings'} /> */}
-                <MenuItemComponent
-                    title="About" icon={IconSubscription}
-                    onClick={() => props.onChange('About')}
-                    active={props.selectedItem === 'About'} />
-            </Column>
-        </Column>
-    );
+class SidebarComponent extends React.Component {
+
+    state = { expanded: false };
+
+    onItemClicked = (item) => {
+        this.setState({ expanded: false });
+        return this.props.onChange(item);
+    }
+
+    isMobile = () => window.innerWidth <= 768;
+
+    toggleMenu = () => this.setState(prevState => ({ expanded: !prevState.expanded }));
+
+    renderBurger = () => {
+        return <div onClick={this.toggleMenu} className={css(styles.burgerIcon)}>
+            <IconBurger />
+        </div>
+    }
+
+    render() {
+        const { expanded } = this.state;
+        const isMobile = this.isMobile();
+        return (
+            <div style={{ position: 'relative' }}>
+                <Row className={css(styles.mainContainer)} breakpoints={{ 768: css(styles.mainContainerMobile) }}>
+                    {(isMobile && !expanded) && this.renderBurger()}
+                    <Column className={css(styles.container)} breakpoints={{ 768: css(styles.containerMobile, expanded ? styles.show : styles.hide) }}>
+                        <LogoComponent />
+                        <Column className={css(styles.menuItemList)}>
+                            <MenuItemComponent
+                                title="Manage Quarter" icon={IconOverview}
+                                onClick={() => this.onItemClicked('Manage Quarter')}
+                                active={this.selectedItem === 'Manage Quarter'}
+                            />
+                            <MenuItemComponent
+                                title="Input Wizard" icon={IconSettings}
+                                onClick={() => this.onItemClicked('Input Wizard')}
+                                active={this.selectedItem === 'Input Wizard'}
+                            />
+                            <MenuItemComponent
+                                title="Dashboard" icon={IconIdeas}
+                                onClick={() => this.onItemClicked('Dashboard')}
+                                active={this.selectedItem === 'Dashboard'} />
+                            <MenuItemComponent
+                                title="Reports" icon={IconArticles}
+                                onClick={() => this.onItemClicked('Reports')}
+                                active={this.selectedItem === 'Reports'} />
+                            <MenuItemComponent
+                                title="About" icon={IconSubscription}
+                                onClick={() => this.onItemClicked('About')}
+                                active={this.props.selectedItem === 'About'}
+                            />
+                           
+                        </Column>
+                    </Column>
+                    {isMobile && expanded && <div className={css(styles.outsideLayer)} onClick={this.toggleMenu}></div>}
+                </Row>
+            </div>
+        );
+    };
 }
 
 export default SidebarComponent;
+
+
+
+
